041b3222117104dfe3f9e72a62e647ee73dd7dd8
João Victor Lima da Silva <joaosilva-ieg@instituto.ad>
2023-10-20 18:46:45 -0300

    arrumando o componente quiz

--- src/Components/Ball.jsx
+++ src/Components/Ball.jsx
@@ -1,15 +1,26 @@
-const Ball = ({selected, index, x, y }) => {
-  const renderArrow = () => {
-    return <polygon points={`${x},${y - 5} ${x - 10},${y} ${x},${y + 5}`} />;
-  };
-
+const Ball = ({ selected, index, x, y }) => {
   return (
     <g>
-      <circle cx={x} cy={y} r="5%" fill={selected ? "#2880F2" : "#ABD0FF" } stroke="#67B5FF" strokeWidth={4} />
-      <text x={x} y={y} fontSize="1.5em" fontWeight="bold" textAnchor="middle" alignmentBaseline="middle" fill="white">
+      <circle
+        cx={x}
+        cy={y}
+        r="5%"
+        fill={selected ? "#2880F2" : "#ABD0FF"}
+        stroke="#67B5FF"
+        strokeWidth={4}
+      />
+      <text
+        x={x}
+        y={y}
+        fontSize="1.5em"
+        fontWeight="bold"
+        textAnchor="middle"
+        alignmentBaseline="middle"
+        fill="white"
+      >
         {index}
       </text>
-      {renderArrow()}
+      <polygon />
     </g>
   );
 };
--- src/Pages/Quiz.jsx
+++ src/Pages/Quiz.jsx
@@ -7,43 +7,42 @@ import module from "../dependencies";
 import { useState } from "react";
 import ImgBell from "../assets/Images/bell.png";
 
-
-export default function Quiz(props) {
+export default function Quiz() {
   
   const dataObject = {
     title: "Exercícios",
     description: "WhatsApp",
-    img: ImgBell
-  }
+    img: ImgBell,
+  };
 
-  const [Clicked, setClicked] = useState(false)
+  const [Clicked, setClicked] = useState(false);
 
   const handleOpen = () => {
-    setClicked(true)
-  }
+    if (Clicked) {
+      return
+    }
+    setClicked(true);
+    setTimeout(handleClose, 1500);
+  };
   const handleClose = () => {
-    setClicked(false)
-  }
-
-  module.useEffect(() => {
-    setTimeout(() => {
-      handleClose()
-    }, "1500");
-  }, [Clicked]);
+    setClicked(false);
+  };
 
   return (
-    <module.Grid sx={{
-      display: "flex",
-      flexDirection: "column",
-      gap: 3
-    }}>
+    <module.Grid
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 3,
+      }}
+    >
       <module.Grid
         sx={{
           backgroundColor: "#2880F2",
           width: "100vw",
           height: "18vh",
           display: "flex",
-          justifyContent: "center"
+          justifyContent: "center",
         }}
       >
         <Tiny
@@ -56,29 +55,33 @@ export default function Quiz(props) {
         />
       </module.Grid>
 
-      <module.Grid sx={{
-        height: "76vh",
-        display: "flex",
-        flexDirection: "column"
-      }}>
-
-        <module.Grid sx={{
-          height: "30%",
+      <module.Grid
+        sx={{
+          height: "76vh",
           display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          padding: "5%"
-        }}>
+          flexDirection: "column",
+        }}
+      >
+        <module.Grid
+          sx={{
+            height: "30%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "5%",
+          }}
+        >
           <Question />
         </module.Grid>
 
-        <module.Grid sx={{
-          height: "70%",
-          display: "flex"
-        }}>
+        <module.Grid
+          sx={{
+            height: "70%",
+            display: "flex",
+          }}
+        >
           <Answer onClick={handleOpen} />
         </module.Grid>
-
       </module.Grid>
       {Clicked ? <Result answer={true} /> : null}
     </module.Grid>
