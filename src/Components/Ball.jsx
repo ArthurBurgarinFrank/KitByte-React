d8f8c5e5c87bf0d5ab5a57864b175410b895b43e
João Victor Lima da Silva <joaosilva-ieg@instituto.ad>
2023-11-06 14:55:54 -0300

    merge de QA para DEV

--- src/Components/Ball.jsx
+++ src/Components/Ball.jsx
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-const Ball = ({ selected, index, x, y }) => {
-  return (
-    <g>
-      <circle
-        cx={x}
-        cy={y}
-        r="5%"
-        fill={selected ? "#2880F2" : "#ABD0FF"}
-        stroke="#67B5FF"
-        strokeWidth={4}
-      />
-      <text
-        x={x}
-        y={y}
-        fontSize="1.5em"
-        fontWeight="bold"
-        textAnchor="middle"
-        alignmentBaseline="middle"
-        fill="white"
-      >
-        {index}
-      </text>
-      <polygon />
-=======
 import module from "../dependencies";
 
 const Ball = ({ selected, index, x, y, img, description }) => {
@@ -53,7 +28,6 @@ const Ball = ({ selected, index, x, y, img, description }) => {
         </text>
         <polygon />
       </module.Link>
->>>>>>> QA
     </g>
   );
 };
