595281fcc9c4138b812ffcef6ba207327ad84220
João Victor Lima da Silva <joaosilva-ieg@instituto.ad>
2023-11-08 16:04:03 -0300

    arrumando footer

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -3,33 +3,56 @@ import inicio from "../assets/Images/inicio.png";
 import ranking from "../assets/Images/ranking.png";
 import tarefas from "../assets/Images/tarefas.png";
 import aulas from "../assets/Images/aulas.png";
+import { useNavigate } from "react-router";
 
 const Footer = () => {
+  const navigate = useNavigate();
+
   return (
     <footer className="footer">
       <div className="footer-button">
-        <a href="/">
+        <module.Button
+          sx={{
+            display: "inline",
+          }}
+          onClick={() => navigate("/")}
+        >
           <img className="imgFooter" src={inicio} alt="Botão 1" />
-          <div className="caption">Início</div>
-        </a>
+        </module.Button>
+        <div className="caption">Início</div>
       </div>
       <div className="footer-button">
-        <a href="/ranking">
-          <img className="imgFooter" src={ranking} alt="Botão 2" />
-          <div className="caption">Ranking</div>
-        </a>
+        <module.Button
+          sx={{
+            display: "inline",
+          }}
+          onClick={() => navigate("/ranking")}
+        >
+          <img className="imgFooter" src={ranking} alt="Botão 1" />
+        </module.Button>
+        <div className="caption">Ranking</div>
       </div>
       <div className="footer-button">
-        <a href="/exercises">
-          <img className="imgFooter" src={tarefas} alt="Botão 3" />
-          <div className="caption">Tarefas</div>
-        </a>
+        <module.Button
+          sx={{
+            display: "inline",
+          }}
+          onClick={() => navigate("/exercises")}
+        >
+          <img className="imgFooter" src={tarefas} alt="Botão 1" />
+        </module.Button>
+        <div className="caption">Tarefas</div>
       </div>
       <div className="footer-button">
-        <a href="/courses">
-          <img className="imgFooter" src={aulas} alt="Botão 4" />
-          <div className="caption">Aulas</div>
-        </a>
+        <module.Button
+          sx={{
+            display: "inline",
+          }}
+          onClick={() => navigate("/courses")}
+        >
+          <img className="imgFooter" src={aulas} alt="Botão 1" />
+        </module.Button>
+        <div className="caption">Aulas</div>
       </div>
     </footer>
   );
