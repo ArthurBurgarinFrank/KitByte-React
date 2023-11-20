acf78e3307c0ea8f539f4a2e88187d2096f75713
João Victor Lima da Silva <joaosilva-ieg@instituto.ad>
2023-11-20 15:09:54 -0300

    terminando a tela de videos

--- src/Pages/Video.jsx
+++ src/Pages/Video.jsx
@@ -1,12 +1,34 @@
 import module from "../dependencies";
+import ReactPlayer from "react-player";
 
 export default function Video() {
+  const [Url, setUrl] = module.useState();
+
   const navigate = module.useNavigate();
+  const location = module.useLocation();
+  const id = location.state ? location.state.id : null;
+  const index = location.state ? location.state.index : null;
+
+  module.useEffect(() => {
+    if (id == 6 && index == 1) {
+      setUrl("https://youtu.be/7RgJZStcnhc?si=6XGZkVQsqpvjBFEF");
+    } else if (id == 6 && index == 2) {
+      setUrl("https://youtu.be/K-qSNH-PxMI?si=y3gOzICeIQ2pkFbx");
+    } else if (id == 4 && index == 1) {
+      setUrl("https://youtu.be/QRQLjvISl2A?si=_2Ql3CS4x9uKTvmG");
+    } else if (id == 4 && index == 2) {
+      setUrl("https://youtu.be/Ku-oQHcFrcc?si=3IYMIOcZkRQr6wWn");
+    }
+  }, [id]);
 
   const handlePage = () => {
-    navigate("/courses")
-  }
-  return (
+    navigate("/courses");
+  };
+
+  console.log(id);
+  console.log(index);
+
+  return !Url ? (
     <module.Dialog
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
@@ -22,5 +44,22 @@ export default function Video() {
         </module.Button>
       </module.DialogActions>
     </module.Dialog>
+  ) : (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <ReactPlayer
+        url={Url}
+        width="65vw"
+        height="65vh"
+        controls={true}
+        playing={true}
+      />
+    </div>
   );
 }
